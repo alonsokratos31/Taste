c1122d10dd04ec98ecdfff268cab66cfb1ef4b31
alonso.garcia025 <alonso.garcia025@gmail.com>
2021-08-15 19:09:29 -0400

    se agrega orde

---
+++ Taste/Controllers/OrderController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+using Taste.Models.ViewModels;
+using Taste.Utility;
+
+namespace Taste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get(string status = null )
+        {
+            List<OrderDetailsViewModel> orderListVM = new List<OrderDetailsViewModel>();
+
+            IEnumerable<OrderHeader> orderHeadersList;
+
+            if (User.IsInRole(SD.CustomerRole))
+            {
+                var claimsdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeadersList = _unitOfWork.OrderHeader.GetAll(u => u.UserId == claim.Value, null, "ApplicationUser");
+            }
+            else            
+                orderHeadersList = _unitOfWork.OrderHeader.GetAll(null, null, "ApplicationUser");
+            
+
+            if (status == "cancelled")
+                orderHeadersList = orderHeadersList.Where(o => o.Status == SD.StatusCancelled || o.Status == SD.StatusRefunded || o.Status == SD.PaymentStatusRejected);
+            
+            else
+            {
+                if (status == "completed")
+                    orderHeadersList = orderHeadersList.Where(o => o.Status == SD.StatusCompleted);
+                
+                else
+                    orderHeadersList = orderHeadersList.Where(o => o.Status == SD.StatusReady || o.Status == SD.StatusInProcess || o.Status == SD.StatusSubmitted || o.Status == SD.PaymentStatusPending);
+                
+            }    
+
+            foreach (OrderHeader item in orderHeadersList)
+            {
+                OrderDetailsViewModel individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetails.GetAll(m => m.OrderId == item.Id).ToList()
+                };
+
+            orderListVM.Add(individual);
+            }
+
+            return Json(new { data = orderListVM });
+        }
+    }
+}
---
+++ Taste/Pages/Admin/Order/ManageOrder.cshtml.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+using Taste.Models.ViewModels;
+using Taste.Utility;
+
+namespace Taste.Pages.Admin.Order
+{
+    public class ManageOrderModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManageOrderModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public List<OrderDetailsViewModel> orderDetailsVM { get; set; }
+
+        public void OnGet()
+        {
+            orderDetailsVM = new List<OrderDetailsViewModel>();
+
+            List<OrderHeader> orderHeaderList = _unitOfWork.OrderHeader
+                                                .GetAll(o => o.Status == SD.StatusSubmitted || o.Status == SD.StatusInProcess)
+                                                .OrderByDescending(U=>U.PickUpTime).ToList();
+
+            foreach (OrderHeader item in orderHeaderList)
+            {
+                OrderDetailsViewModel individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetails.GetAll(m => m.OrderId == item.Id).ToList()
+                };
+
+                orderDetailsVM.Add(individual);
+            }
+
+        }
+
+
+        public IActionResult OnPostOrderPrepare(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            orderHeader.Status = SD.StatusInProcess;
+            _unitOfWork.Save();
+            return RedirectToPage("ManageOrder");
+        }
+
+        public IActionResult OnPostOrderReady(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            orderHeader.Status = SD.StatusReady;
+            _unitOfWork.Save();
+            return RedirectToPage("ManageOrder");
+        }
+
+        public IActionResult OnPostOrderCancel(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            orderHeader.Status = SD.StatusCancelled;
+            _unitOfWork.Save();
+            return RedirectToPage("ManageOrder");
+        }
+
+        public IActionResult OnPostOrderOrderRefund(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+            //refund ammount
+
+            var options = new RefundCreateOptions
+            {
+                Amount = Convert.ToInt32(orderHeader.OrderTotal*100),
+                Reason = RefundReasons.RequestedByCustomer,
+                Charge = orderHeader.Transaction
+            };
+
+            var service = new RefundService();
+            service.Create(options);
+
+            orderHeader.Status = SD.StatusRefunded;
+            _unitOfWork.Save();
+            return RedirectToPage("ManageOrder");
+        }
+
+    }
+}
